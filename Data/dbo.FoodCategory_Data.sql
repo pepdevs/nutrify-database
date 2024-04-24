7b49a0d577ac2dca43100593783b8b289c716179
Pep Soler <jsoler@delsys.net>
2024-04-24 12:41:25 +0200

    Added static data

---
+++ Data/dbo.FoodCategory_Data.sql
@@ -0,0 +1,9 @@
+SET IDENTITY_INSERT [dbo].[FoodCategory] ON
+INSERT INTO [dbo].[FoodCategory] ([Id], [Name]) VALUES (1, N'Fruits')
+INSERT INTO [dbo].[FoodCategory] ([Id], [Name]) VALUES (2, N'Vegetables')
+INSERT INTO [dbo].[FoodCategory] ([Id], [Name]) VALUES (3, N'Protein')
+INSERT INTO [dbo].[FoodCategory] ([Id], [Name]) VALUES (4, N'Nuts')
+INSERT INTO [dbo].[FoodCategory] ([Id], [Name]) VALUES (5, N'Starches')
+INSERT INTO [dbo].[FoodCategory] ([Id], [Name]) VALUES (6, N'Dairy')
+INSERT INTO [dbo].[FoodCategory] ([Id], [Name]) VALUES (7, N'Others')
+SET IDENTITY_INSERT [dbo].[FoodCategory] OFF
