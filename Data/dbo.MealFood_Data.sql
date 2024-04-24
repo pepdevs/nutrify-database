006baae7da1f08ac63ef6d03181c01a98dbf8157
Pep Soler <jsoler@delsys.net>
2024-04-24 16:13:49 +0200

    Edit foods nutritional values

---
+++ Data/dbo.MealFood_Data.sql
@@ -0,0 +1,11 @@
+SET IDENTITY_INSERT [dbo].[MealFood] ON
+INSERT INTO [dbo].[MealFood] ([Id], [MealId], [FoodId], [Amount]) VALUES (1, 1, 3, 75)
+INSERT INTO [dbo].[MealFood] ([Id], [MealId], [FoodId], [Amount]) VALUES (2, 1, 1, 75)
+INSERT INTO [dbo].[MealFood] ([Id], [MealId], [FoodId], [Amount]) VALUES (3, 1, 46, 20)
+INSERT INTO [dbo].[MealFood] ([Id], [MealId], [FoodId], [Amount]) VALUES (4, 2, 35, 130)
+INSERT INTO [dbo].[MealFood] ([Id], [MealId], [FoodId], [Amount]) VALUES (5, 2, 45, 25)
+INSERT INTO [dbo].[MealFood] ([Id], [MealId], [FoodId], [Amount]) VALUES (6, 3, 50, 120)
+INSERT INTO [dbo].[MealFood] ([Id], [MealId], [FoodId], [Amount]) VALUES (7, 3, 40, 200)
+INSERT INTO [dbo].[MealFood] ([Id], [MealId], [FoodId], [Amount]) VALUES (8, 3, 46, 20)
+INSERT INTO [dbo].[MealFood] ([Id], [MealId], [FoodId], [Amount]) VALUES (9, 3, 58, 15)
+SET IDENTITY_INSERT [dbo].[MealFood] OFF
---
+++ Data/dbo.Meal_Data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[Meal] ON
+INSERT INTO [dbo].[Meal] ([Id], [Date]) VALUES (1, '2024-04-24 07:00:00.0000000')
+INSERT INTO [dbo].[Meal] ([Id], [Date]) VALUES (2, '2024-04-24 11:00:00.0000000')
+INSERT INTO [dbo].[Meal] ([Id], [Date]) VALUES (3, '2024-04-24 13:30:00.0000000')
+SET IDENTITY_INSERT [dbo].[Meal] OFF
