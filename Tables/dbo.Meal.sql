489899b41a02f3f7a461ce35d14ad9db090ee7e3
Pep Soler <jsoler@delsys.net>
2024-04-24 12:48:57 +0200

    Modified Meal Date field to datetime2

--- Tables/dbo.Meal.sql
+++ Tables/dbo.Meal.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[Meal]
 (
 [Id] [int] NOT NULL IDENTITY(1, 1),
-[Date] [date] NOT NULL
+[Date] [datetime2] NOT NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Meal] ADD CONSTRAINT [PK__Meal__3214EC0790C57AD4] PRIMARY KEY CLUSTERED ([Id]) ON [PRIMARY]
