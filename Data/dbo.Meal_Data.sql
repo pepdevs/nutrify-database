ba0a56d2d38ba023415b47bd28c42449bd76e061
Pep Soler <jsoler@delsys.net>
2024-07-22 11:45:14 +0200

    (feat) Added data

--- Data/dbo.Meal_Data.sql
+++ Data/dbo.Meal_Data.sql
@@ -2,4 +2,9 @@ SET IDENTITY_INSERT [dbo].[Meal] ON
 INSERT INTO [dbo].[Meal] ([Id], [Date]) VALUES (1, '2024-04-24 07:00:00.0000000')
 INSERT INTO [dbo].[Meal] ([Id], [Date]) VALUES (2, '2024-04-24 11:00:00.0000000')
 INSERT INTO [dbo].[Meal] ([Id], [Date]) VALUES (3, '2024-04-24 13:30:00.0000000')
+INSERT INTO [dbo].[Meal] ([Id], [Date]) VALUES (4, '2024-04-29 08:00:00.0000000')
+INSERT INTO [dbo].[Meal] ([Id], [Date]) VALUES (5, '2024-05-17 11:00:00.0000000')
+INSERT INTO [dbo].[Meal] ([Id], [Date]) VALUES (11, '2024-06-01 08:30:00.0000000')
+INSERT INTO [dbo].[Meal] ([Id], [Date]) VALUES (12, '2024-06-01 13:30:00.0000000')
+INSERT INTO [dbo].[Meal] ([Id], [Date]) VALUES (13, '2024-06-01 18:30:00.0000000')
 SET IDENTITY_INSERT [dbo].[Meal] OFF
